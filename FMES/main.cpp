d45f22686c39706f0a175ca42ba22a77e8fb4393
arksenu <leizaro.fei@gmail.com>
2022-02-15 11:14:01 -0800

    Initial Commit

---
+++ FMES/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FMES
+//
+//  Created by Dario Mazhara on 2/15/22.
+//  Copyright © 2022 Dario Mazhara. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
